bb1683fb61cec2e3435a3eaf6f2bf86783340bd3
Tom Lee <thomas.j.lee@gmail.com>
2014-11-11 11:55:19 -0500

    initial commit, v 1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+=== Uber City Collector
+
+Scrapes information from Uber.com's list of current cities and stores it in JSON files for further processing.
+
+Please understand your responsibilities and liabilities under Uber's [Terms of Service](https://www.uber.com/en-US/legal/usa/terms) for your country before using this software.
---
+++ index.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+var limit = require('simple-rate-limiter');
+var request = limit(require('request')).to(5).per(1000);
+
+request('https://www.uber.com/en-US/cities', function(err, resp, html){
+    var $ = cheerio.load(html);
+    $('a')
+        .filter(function(i, elem) { 
+            return elem.attribs.href.match(/\/cities\//); 
+        })
+        .each(function(i, elem){
+            var cityName = elem.attribs.href.replace(/\/cities\//, '').replace(/\/$/, '');
+            request('https://www.uber.com' + elem.attribs.href, function(err, resp, html){
+                var $$ = cheerio.load(html);
+                $$('script')
+                    .filter(function(i, elem){
+                        return (elem.children && elem.children[0] && elem.children[0].data && elem.children[0].data.match(/cityJSON/));
+                    })
+                    .each(function(i, elem){                        
+                        eval(elem.children[0].data);
+                        fs.writeFile('cities/' + cityName + '.json', JSON.stringify(cityJSON), function(err) {
+                            if (err) console.log('ERROR: ' + err);
+                            console.log(' - ' + cityName);
+                        });
+                    });
+            });
+        });
+});
